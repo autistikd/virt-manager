116450abeabbf583698eeb54a36df4ba2e16c47f
autistikd <autistikbarn@proton.me>
2025-01-21 20:15:01 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 xbps-install -y libvirt virt-manager qemu polkit
-usermod -aG libvirt,kvm void
+usermod -aG libvirt,kvm anon
 for dir in images isos; do
-    mkdir -p /home/void/libvirt/$dir
+    mkdir -p /home/anon/libvirt/$dir
 done
-chown :kvm /home/void/libvirt/images
+chown :kvm /home/anon/libvirt/images
 rmdir /var/lib/libvirt/images
-ln -s /home/void/libvirt/images /var/lib/libvirt/images
-ln -s /home/void/libvirt/isos /var/lib/libvirt/isos
-mkdir /home/void/.config/libvirt
-cp -rv /etc/libvirt/libvirt.conf /home/void/.config/libvirt/
+ln -s /home/anon/libvirt/images /var/lib/libvirt/images
+ln -s /home/anon/libvirt/isos /var/lib/libvirt/isos
+mkdir /home/anon/.config/libvirt
+cp -rv /etc/libvirt/libvirt.conf /home/anon/.config/libvirt/
 sed -i 's|^[[:space:]]*#uri_default = "qemu:///system"[[:space:]]*$|uri_default = "qemu:///system"|' /home/void/.config/libvirt/libvirt.conf
-sed -i -e 's|^#user = "libvirt"|user = "void"|' -e 's|^#group= "libvirt"|group = "libvirt"|' /etc/libvirt/qemu.conf
+sed -i -e 's|^#user = "libvirt"|user = "anon"|' -e 's|^#group= "libvirt"|group = "libvirt"|' /etc/libvirt/qemu.conf
 ln -s /etc/sv/libvirtd /var/service/
 ln -s /etc/sv/virtlockd /var/service/
 ln -s /etc/sv/virtlogd /var/service/
