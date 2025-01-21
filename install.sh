183ae5b90acce19f99ebc8d02f2254b169ac841c
autistikd <autistikbarn@proton.me>
2025-01-21 20:43:33 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ ln -s /home/anon/libvirt/images /var/lib/libvirt/images
 ln -s /home/anon/libvirt/isos /var/lib/libvirt/isos
 mkdir /home/anon/.config/libvirt
 cp -rv /etc/libvirt/libvirt.conf /home/anon/.config/libvirt/
-sed -i 's|^[[:space:]]*#uri_default = "qemu:///system"[[:space:]]*$|uri_default = "qemu:///system"|' /home/void/.config/libvirt/libvirt.conf
+sed -i 's|^[[:space:]]*#uri_default = "qemu:///system"[[:space:]]*$|uri_default = "qemu:///system"|' /home/anon/.config/libvirt/libvirt.conf
 sed -i -e 's|^#user = "libvirt"|user = "anon"|' -e 's|^#group= "libvirt"|group = "libvirt"|' /etc/libvirt/qemu.conf
 ln -s /etc/sv/libvirtd /var/service/
 ln -s /etc/sv/virtlockd /var/service/
